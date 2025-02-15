541e9407e0fd814f6c836cb8188bee85bd597070
chientrm <chientrm@gmail.com>
2025-02-15 19:33:31 +0700

    f

--- src/vicsek_fractal.c
+++ src/vicsek_fractal.c
@@ -27,6 +27,29 @@ void drawVicsekFractal(unsigned char *image, int x, int y, int size, int order, 
     }
 }
 
+void rotateImage45Degrees(unsigned char *src, unsigned char *dst, int width, int height) {
+    int newWidth = width * 1.414; // sqrt(2) * width
+    int newHeight = height * 1.414; // sqrt(2) * height
+    int cx = width / 2;
+    int cy = height / 2;
+    int ncx = newWidth / 2;
+    int ncy = newHeight / 2;
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int nx = (x - cx) * 0.707 - (y - cy) * 0.707 + ncx;
+            int ny = (x - cx) * 0.707 + (y - cy) * 0.707 + ncy;
+            if (nx >= 0 && nx < newWidth && ny >= 0 && ny < newHeight) {
+                int srcIndex = (y * width + x) * 3;
+                int dstIndex = (ny * newWidth + nx) * 3;
+                dst[dstIndex] = src[srcIndex];
+                dst[dstIndex + 1] = src[srcIndex + 1];
+                dst[dstIndex + 2] = src[srcIndex + 2];
+            }
+        }
+    }
+}
+
 int main() {
     unsigned char *image = (unsigned char *)calloc(WIDTH * HEIGHT * 3, sizeof(unsigned char));
     if (!image) {
@@ -39,12 +62,25 @@ int main() {
 
     drawVicsekFractal(image, 0, 0, WIDTH, order, color);
 
-    if (!stbi_write_png("images/vicsek_fractal.png", WIDTH, HEIGHT, 3, image, WIDTH * 3)) {
-        fprintf(stderr, "Failed to write image\n");
+    int newWidth = WIDTH * 1.414; // sqrt(2) * WIDTH
+    int newHeight = HEIGHT * 1.414; // sqrt(2) * HEIGHT
+    unsigned char *rotatedImage = (unsigned char *)calloc(newWidth * newHeight * 3, sizeof(unsigned char));
+    if (!rotatedImage) {
+        fprintf(stderr, "Failed to allocate memory for rotated image\n");
         free(image);
         return 1;
     }
 
+    rotateImage45Degrees(image, rotatedImage, WIDTH, HEIGHT);
+
+    if (!stbi_write_png("images/vicsek_fractal_rotated.png", newWidth, newHeight, 3, rotatedImage, newWidth * 3)) {
+        fprintf(stderr, "Failed to write image\n");
+        free(image);
+        free(rotatedImage);
+        return 1;
+    }
+
     free(image);
+    free(rotatedImage);
     return 0;
 }
