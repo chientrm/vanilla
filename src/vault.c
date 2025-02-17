56c0b2a4a449c8a0a19ea185bfd3a33320ebab12
chientrm <chientrm@gmail.com>
2025-02-17 11:15:01 +0700

    cube vault.c

---
+++ src/vault.c
@@ -0,0 +1,124 @@
+#include <GL/glut.h>
+
+float angleX = 0.0f;
+float angleY = 0.0f;
+int lastMouseX, lastMouseY;
+int isDragging = 0;
+
+void initGL() {
+    glEnable(GL_DEPTH_TEST);
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -5.0f);
+    glRotatef(angleX, 1.0f, 0.0f, 0.0f);
+    glRotatef(angleY, 0.0f, 1.0f, 0.0f);
+
+    glBegin(GL_QUADS);
+    // Front face
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    // Back face
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    // Top face
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    // Bottom face
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    // Right face
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    // Left face
+    glColor3f(0.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void reshape(int width, int height) {
+    if (height == 0) height = 1;
+    float aspect = (float)width / (float)height;
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void mouseMotion(int x, int y) {
+    if (isDragging) {
+        angleX += (y - lastMouseY);
+        angleY += (x - lastMouseX);
+        lastMouseX = x;
+        lastMouseY = y;
+        glutPostRedisplay();
+    }
+}
+
+void mouseButton(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON) {
+        if (state == GLUT_DOWN) {
+            isDragging = 1;
+            lastMouseX = x;
+            lastMouseY = y;
+        } else {
+            isDragging = 0;
+        }
+    }
+}
+
+void keyboard(int key, int x, int y) {
+    switch (key) {
+        case GLUT_KEY_UP:
+            angleX -= 5.0f;
+            break;
+        case GLUT_KEY_DOWN:
+            angleX += 5.0f;
+            break;
+        case GLUT_KEY_LEFT:
+            angleY -= 5.0f;
+            break;
+        case GLUT_KEY_RIGHT:
+            angleY += 5.0f;
+            break;
+    }
+    glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("OpenGL Cube with Mouse Navigation");
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMotionFunc(mouseMotion);
+    glutMouseFunc(mouseButton);
+    glutSpecialFunc(keyboard);
+    initGL();
+    glutMainLoop();
+    return 0;
+}
